4f2d0b5071cb668bb8e608681241faac5779ee59
Amingman <stefanuslie91@gmail.com>
2023-04-03 12:46:17 +0700

    Update config for database env

---
+++ config/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+
+let missing = ["PORT", "DATABASE_URL", "MAPS_API_KEY"].filter(
+  key => process.env[key] === undefined
+)
+
+if (missing.length > 0) {
+  throw Error(`missing environment variables for ${missing.join(", ")}`)
+}
+
+module.exports = {
+  port: process.env.PORT,
+  db: {
+    connectionString: process.env.DATABASE_URL,
+  },
+  apiKey: process.env.MAPS_API_KEY
+}
