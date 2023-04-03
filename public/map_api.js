bff92e3a38aa7598b2209b510ebabb51b61bfc1c
Amingman <stefanuslie91@gmail.com>
2023-04-03 14:36:27 +0700

    Added 400 markers to map

--- public/map_api.js
+++ public/map_api.js
@@ -1,11 +1,31 @@
 let map;
 
 async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
   });
+
+  fetchPetrolStations()
+    .then(stations => {
+      stations.forEach(station => {
+        const myLatLng = { lat: Number(station[`latitude`]), lng: Number(station[`longitude`]) };
+    
+        new google.maps.Marker({
+          position: myLatLng,
+          map,
+          // title: "Hello World!",
+        });
+      });
+    })
+
+
+}
+
+
+
+function fetchPetrolStations() {
+  return axios.get(`/api/stations/all`).then(res => res.data)   
 }
