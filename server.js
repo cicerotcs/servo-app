84c7cde04159ef5573d80e817f474f55dc74d3c6
Cicero Teixeira <ccrtxr@gmail.com>
2023-04-03 12:19:09 +0930

    added css to homepage

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use("/api");
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 const port = process.env.PORT || 8080;
 
